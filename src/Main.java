fe8609a7d2b51abce84e467c40f5885f130543ef
wanderinight <115969449+wanderinight@users.noreply.github.com>
2025-08-03 22:47:55 +0800

    array_try

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
+import java.util.Arrays;
+
+
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        System.out.println("initial list :  " + list);
+        System.out.println("initial size : " + list.size());
 
+        for (int i = 0; i < 15; i++) {
+            list.add(i * 2); // 测试自动扩容
+        }
+        System.out.println("afteradd list :  " + list);
+        System.out.println("afteradd size : " + list.size());
+
+        list.add(3,666);
+        System.out.println("afterinsert list :  " + list);
+        System.out.println("afterinsert size : " + list.size());
+
+        System.out.println("search index 10 :  " + list.indexOf(10));
+
+        int removed = list.remove(5);
+        System.out.println("Removed element: " + removed);
+        System.out.println("After removal: " + list);
+
+        // 搜索元素
+        int index = list.indexOf(20);
+        System.out.println("Index of 20: " + index);
+
+        // 边界测试
+        try {
+            list.add(100, 100); // 越界插入
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Caught: " + e.getMessage());
+        }
     }
 }
--- src/basicarray.java
+++ src/basicarray.java
@@ -1,3 +1,27 @@
-public class basicarray {
-    int a;
-}
+//import java.util.Arrays;
+//新建--插入--查询--二分查找--delete同理
+//
+//int[] arr =new int[10];
+//String[] strArr ={"A","B","C"};
+//
+//        System.out.println(arr[0]);
+//        System.out.println(strArr[1]);
+//
+//arr[1]=10;
+//        System.out.println("arr[1]:"+arr[1]);
+//
+//        Arrays.sort(arr);
+//int index= Arrays.binarySearch(arr,10);
+//        System.out.println("index of 10 at"+index);
+//
+//int[] newArr=insertElement(arr,99,3);
+//        System.out.println(Arrays.toString(newArr));
+//        }
+//
+//public static int[] insertElement(int[] arr, int element, int index) {
+//    int[] newArr=new int[arr.length+1];
+//    System.arraycopy(arr,0,newArr,0, index);
+//    newArr[index]=element;
+//    System.arraycopy(arr, index, newArr, index + 1, arr.length - index);
+//    return newArr;
+//}
